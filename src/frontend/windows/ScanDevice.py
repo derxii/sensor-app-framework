688f74af05532552ea647a10c2025f413d72ae0e
Derrick <152702547+derxii@users.noreply.github.com>
2025-01-21 10:22:59 +1100

    add data for testing scan

--- src/frontend/windows/ScanDevice.py
+++ src/frontend/windows/ScanDevice.py
@@ -1,9 +1,9 @@
 from PySide6.QtWidgets import QVBoxLayout, QWidget, QLabel
-from PySide6.QtGui import QMovie, Qt, QFont
-from PySide6.QtCore import QSize, QTimer
+from PySide6.QtGui import Qt, QFont
+from PySide6.QtCore import QTimer
 from typing import Callable
 
-from frontend.helper import get_image_path
+from frontend.widgets.Loader import Loader
 from frontend.windows.Devices import Devices
 from frontend.windows.ScrollableWindow import ScrollableWindow
 
@@ -12,8 +12,7 @@ class ScanDevice(ScrollableWindow):
     def __init__(self, switch_window: Callable[[QWidget], None]):
         super().__init__(switch_window)
         self.title = QLabel("Scanning for Bluetooth Devices")
-        self.loading_animation = QLabel()
-        self.movie = QMovie(get_image_path("spinner.gif"))
+        self.loader = Loader(256)
         self.description = QLabel("Please wait for up to 5 seconds...")
         self.init_ui()
 
@@ -25,17 +24,13 @@ class ScanDevice(ScrollableWindow):
 
         title_font = QFont()
         title_font.setPointSize(40)
-        title_font.setWeight(QFont.Weight.Medium)
+        title_font.setWeight(QFont.Weight.DemiBold)
         self.title.setFont(title_font)
         self.title.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(self.title)
 
-        self.movie.setScaledSize(QSize(256, 256))
-        self.loading_animation.setMovie(self.movie)
-        self.movie.start()
-        self.loading_animation.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        layout.addWidget(self.loading_animation)
+        self.loader.start_animation()
+        layout.addWidget(self.loader)
 
         description_font = QFont()
         description_font.setPointSize(20)
@@ -46,10 +41,11 @@ class ScanDevice(ScrollableWindow):
         self.bind_scroll(layout)
 
     def trigger_bluetooth_scan(self):
-        QTimer.singleShot(5000, self.on_scan_end)
+        QTimer.singleShot(1000, self.on_scan_end)
 
     def on_scan_end(self):
-        self.movie.stop()
-        self.movie.deleteLater()
-        dummy_data = [("Arduino HC-06", "M9SK1K31-252D-43E3-A986-DCF3CB63D08", -50) for _ in range(25)]
+        self.loader.stop_animation()
+        dummy_data = [("Arduino HC-06", "M9SK1K31-252D-43E3-A986-DCF3CB63D08", -50) for _ in range(33)]
+        dummy_data += [("long device name", "NDKA92N-24124-1241", -80)]
+
         self.switch_window(Devices(self.switch_window, dummy_data))
