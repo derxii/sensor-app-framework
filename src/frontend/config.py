bee690aa90b6eb2a322c5d300f330bc16699c7f6
Derrick <152702547+derxii@users.noreply.github.com>
2025-01-20 11:36:21 +1100

    rename folder

--- src/frontend/config.py
+++ src/frontend/config.py
@@ -1,8 +1,17 @@
 from pathlib import Path
+from PySide6.QtWidgets import QStyleOption, QStyle, QWidget
+from PySide6.QtGui import QPainter
 
-images_dir = Path(__file__).parent.parent.joinpath("images")
+resources_dir = Path(__file__).parent.parent.joinpath("resources")
 default_width = 960
 default_height = 576
 
 def get_image_path(name: str):
-    return str(images_dir.joinpath(name))
+    return str(resources_dir.joinpath(name))
+
+
+def enable_custom_styling(widget: QWidget):
+    opt = QStyleOption()
+    opt.initFrom(widget) 
+    painter = QPainter(widget)
+    widget.style().drawPrimitive(QStyle.PE_Widget, opt, painter, widget)
