c345ec0866e702cc69776ad87e27bf2fbf6bd656
Derrick <152702547+derxii@users.noreply.github.com>
2025-01-23 11:16:05 +1100

    move debug data

---
+++ src/frontend/DebugData.py
@@ -0,0 +1,6 @@
+def get_debug_scan_devices():
+    data = [
+        ("Arduino HC-06", "M9SK1K31-252D-43E3-A986-DCF3CB63D08", -50) for _ in range(33)
+    ]
+    data += [("long device name", "NDKA92N-24124-1241", 9)]
+    return data
--- src/frontend/config.py
+++ src/frontend/config.py
@@ -44,15 +44,6 @@ def is_debug():
     args = parser.parse_args()
     return args.debug
 
-
-def get_debug_scan_devices():
-    data = [
-        ("Arduino HC-06", "M9SK1K31-252D-43E3-A986-DCF3CB63D08", -50) for _ in range(33)
-    ]
-    data += [("long device name", "NDKA92N-24124-1241", 9)]
-    return data
-
-
 def set_backend():
     QCoreApplication.instance().setProperty("backend", Backend())
 
