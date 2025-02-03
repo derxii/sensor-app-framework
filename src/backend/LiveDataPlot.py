3a8840641602270ee220bc5484509a1f61558197
5366491 <134902922+5366491@users.noreply.github.com>
2025-02-03 13:45:23 +1100

    Added heatmap and changed previous heatmap code to matrix

--- src/backend/LiveDataPlot.py
+++ src/backend/LiveDataPlot.py
@@ -19,7 +19,8 @@ class LiveDataPlot(QMainWindow):
         self.setWindowTitle("Live Data Plotting with PyQt6")
         self.setGeometry(100, 100, 800, 600)
         self.allPlots = []
-        self.allHeatmaps = [] # List of dictionaries that include chart id, sensor and plot
+        self.allMatrices = [] # List of dictionaries that include chart id, sensor and plot
+        self.allHeatmaps = []
         # Set up the central widget and layout
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
@@ -59,17 +60,14 @@ class LiveDataPlot(QMainWindow):
                 dock.setWidget(plot)
                 self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, dock)
 
-            if chart.getType() == "heatmap":
+            if chart.getType() == "matrix":
                 # Create ImageView for heatmap
                 dock = QDockWidget(chart.getTitle(), self)
                 widget = QWidget()
                 dock.setWidget(widget)
-                heatmapLayout = QVBoxLayout(widget)
+                matrixLayout = QVBoxLayout(widget)
                 imageView = pg.ImageView()
-                heatmapLayout.addWidget(imageView)
-
-                
-                
+                matrixLayout.addWidget(imageView)
 
                 #layout.addWidget(imageView)
                 colormap = pg.colormap.get('inferno')  # Choose a color map
@@ -80,17 +78,47 @@ class LiveDataPlot(QMainWindow):
                 (min, max) = chart.getMinMaxRange()
                 image_item.setLevels([min, max])  # Set min-max temperature range
 
-                sensors = chart.getSensors()
-                heatmapDict = {"chartId":chart.getId(), "imageView": imageView, "sensors": sensors , "numSensors": len(sensors), "N": int(math.sqrt(len(sensors)))}
-                self.allHeatmaps.append(heatmapDict)
+                sensors = sorted(chart.getSensors())
+                matrixDict = {"chartId":chart.getId(), "imageView": imageView, "sensors": sensors , "numSensors": len(sensors), "N": int(math.sqrt(len(sensors)))}
+                self.allMatrices.append(matrixDict)
                 self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea,dock)
-                #dock = QDockWidget(chart.getTitle(), self)
-                #dock.setWidget(plot)
-                #self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, dock)
-                #layout.addWidget(imageView)
-                
 
                 
+            if chart.getType() == "heatmap":
+                dock = QDockWidget(chart.getTitle(), self)
+                widget = QWidget()
+                dock.setWidget(widget)
+                heatmapLayout = QVBoxLayout(widget)
+                plot_item = pg.PlotItem()
+                imageView = pg.ImageView(view=plot_item)
+                heatmapLayout.addWidget(imageView)
+
+                #layout.addWidget(imageView)
+                colormap = pg.colormap.get('inferno')  # Choose a color map
+                lut = colormap.getLookupTable()  # Generate lookup table
+                # Set the image with correct LUT
+                image_item = imageView.getImageItem()  # Access the internal ImageItem
+                image_item.setLookupTable(lut)  # Apply color map
+                (min, max) = chart.getMinMaxRange()
+                image_item.setLevels([min, max])  # Set min-max temperature range
+
+                categories = chart.getCategories()
+                #Create data struct
+                
+                sensors = sorted(chart.getSensors())
+                # Set axis labels 
+                
+                plot_item.setLabel('left', chart.getyLabel())
+                plot_item.setLabel('bottom', chart.getxLabel())
+                x_ticks = [(i, sensors[i]) for i in range(len(sensors))]
+                y_ticks = [(i, f"{categories[i][0]}-{categories[i][1]}") for i in range(len(categories))]
+                plot_item.getAxis('bottom').setTicks([x_ticks])
+                plot_item.getAxis('left').setTicks([y_ticks])
+
+                dataStruct = np.zeros((len(categories), len(sensors)), dtype=float)
+                heatmapDict = {"chartId":chart.getId(), "imageView": imageView, "sensors": sensors , "numSensors": len(sensors), "categories": categories, "numCategories": len(categories), "data": dataStruct}
+                self.allHeatmaps.append(heatmapDict)
+                self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea,dock)
                 
         
         self.is_paused = False
@@ -109,6 +137,7 @@ class LiveDataPlot(QMainWindow):
       
     def toggle_pause(self):
         self.is_paused = not self.is_paused
+        self.Backend.togglePause()
         self.pause_button.setText("Resume" if self.is_paused else "Stop")   
 
     def update_plot(self):
@@ -128,15 +157,34 @@ class LiveDataPlot(QMainWindow):
                     plotLine.setData(self.allPlots[i]["dataStream"][sensor]["xData"], self.allPlots[i]["dataStream"][sensor]["yData"])
                     self.allPlots[i]["dataStream"][sensor]["line"] = plotLine
                     
-            for i in range(0, len(self.allHeatmaps)):
-                chartId = int(self.allHeatmaps[i]["chartId"])
+            for i in range(0, len(self.allMatrices)):
+                chartId = int(self.allMatrices[i]["chartId"])
+                chart = self.Backend.getChart(chartId)
+                #if not chart.isQueueReady():
+                #    continue
                 chart = self.Backend.getChart(chartId)
                 data = [] 
-                for sensor in self.allHeatmaps[i]["sensors"]:
+                for sensor in self.allMatrices[i]["sensors"]:
                     data.append(float(chart.getLastDataPoint(sensor)))
-                structuredData = np.resize(np.array(data), (self.allHeatmaps[i]["N"], self.allHeatmaps[i]["N"]))
-                self.allHeatmaps[i]["imageView"].setImage(structuredData.T)
+                structuredData = np.resize(np.array(data), (self.allMatrices[i]["N"], self.allMatrices[i]["N"]))
+                self.allMatrices[i]["imageView"].setImage(structuredData.T)
 
+            for i in range(0, len(self.allHeatmaps)):
+                categories = self.allHeatmaps[i]["categories"]
+                sensors = self.allHeatmaps[i]["sensors"]
+                data = self.allHeatmaps[i]["data"]
+                chartId = int(self.allMatrices[i]["chartId"])
+                chart = self.Backend.getChart(chartId)
+                for j in range(0, len(sensors)):
+                    dataVal = chart.getLastDataPoint(sensors[j])
+                    if dataVal is None:
+                        continue
+                    dataVal = float(dataVal)
+                    for k in range(0, len(categories)):
+                        if dataVal >= categories[k][0] and dataVal <= categories[k][1]:
+                            data[k][j] += 1
+                self.allHeatmaps[i]["data"] = data
+                self.allHeatmaps[i]["imageView"].setImage(data.T)
 
     def clearPlots(self):
         for i in range(0,len(self.allPlots)): 
@@ -147,6 +195,9 @@ class LiveDataPlot(QMainWindow):
                 plotLine = self.allPlots[i]["dataStream"][sensor]["line"]
                 plotLine.setData(self.allPlots[i]["dataStream"][sensor]["xData"], self.allPlots[i]["dataStream"][sensor]["yData"])
                 self.allPlots[i]["dataStream"][sensor]["line"] = plotLine
+    
+    def livePlotExists(self):
+        return len(self.allPlots) + len(self.allMatrices) != 0
 
 class LiveHeatMap(QMainWindow):
     def __init__(self, backend):
